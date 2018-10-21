f036d733aec5962d9e9108282b55343bf07b09f6
poppy <poppy5gungan@gmail.com>
2018-10-21 22:43:50 +0100

    .

--- loop.sh
+++ loop.sh
@@ -2,7 +2,7 @@
 
 echo "Starting ssh loop"
 
-while :; do
+#while :; do
   sshpass -f /pw.txt ssh -R 33223:localhost:22 -p 33333 -o "StrictHostKeyChecking no" dock@new5.coinshak.com
   sleep 5s
-done
+#done
